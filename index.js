7d7cd79372db9a7706b99ed3641dc3da177650a1
Brittany Malooly <bmalooly@gmail.com>
2019-01-05 23:12:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name)
 }
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten() {
+  return 
